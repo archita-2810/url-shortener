919cee8b3a9e1bb364c6a83501fa27cd78ad81ef
archita-2810 <architasugandh@gmail.com>
2025-03-21 17:38:22 +0530

    completed by adding frontend and analytics table

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { connectToMongoDb } = require("./connection");
 
 const urlRoute = require("./routes/url");
+const path = require("path");
 const URL = require("./models/url");
 const app = express();
 const PORT = 8001;
@@ -9,11 +10,18 @@ const PORT = 8001;
 connectToMongoDb("mongodb://127.0.0.1:27017/short-url").then(() =>
   console.log("mongo connected.")
 );
+
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./views"))
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const staticRoute = require('./routes/staticRoutes');
+
+app.use("/", staticRoute);
 
 app.use("/url", urlRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
